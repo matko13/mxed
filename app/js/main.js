6b664d2b9130f1604eb8bc9cc9fa80bf9378b3a3
Krzysztof Lubiniecki <klubiniecki@gmail.com>
2017-09-07 22:28:59 +0200

    add logos and hero elements

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,12 @@
 document.onreadystatechange = function () {
   if (document.readyState === "complete") {
-      console.log('ready');
+    const ACTIVE_CLASS = 'is-active';
+  
+    const dropdown = document.querySelector('.js-burger');
+  
+    dropdown.addEventListener('click', (e) => {
+      e.stopPropagation();  
+      dropdown.classList.toggle('is-active');
+    });
   }
 }
