83aea6c220d57ec2c9bddece208b37b8138a68a8
Krzysztof Lubiniecki <klubiniecki@gmail.com>
2017-09-12 07:27:36 +0200

    Add autoprefixer, cssnano and sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
+'use strict';
+
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
+const cssnano = require('gulp-cssnano');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('browserSync', function() {
   browserSync.init({
@@ -10,17 +15,21 @@ gulp.task('browserSync', function() {
   })
 });
 
-gulp.task('sass', function(){
-  return gulp.src('app/scss/style.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('app/css'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
+gulp.task('workflow', function () {
+	gulp.src('app/scss/**/*.scss')
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(cssnano())
+		.pipe(sourcemaps.write('./'))
+	.pipe(gulp.dest('app/css'))
 });
 
-gulp.task('watch', ['browserSync'], function(){
-  gulp.watch('app/scss/**/*.scss', ['sass']);
+gulp.task('watch', ['browserSync'], function () {
+  gulp.watch('app/scss/**/*.scss', ['workflow']);
   gulp.watch('index.html', browserSync.reload);
   gulp.watch('app/js/main.js', browserSync.reload);
 });
