a2ebb18fb4dfa2720579e8b8f37c9a238ec00d22
Krzysztof Lubiniecki <klubiniecki@gmail.com>
2017-09-14 06:21:40 +0200

    UI updates

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,16 +4,26 @@ document.onreadystatechange = function () {
     const header = document.querySelector('.header__wrapper');
     const dropdown = document.querySelector('.js-burger');
     const menu = document.querySelector('.header__nav-list');
-    const menuItem = Array.from(document.querySelectorAll('.js-menu'));
     const slider = document.querySelector('.js_slider');
+    const menuButtons = Array.from(document.querySelectorAll('.js-button'));
     lory(slider, { infinite: 1 });
 
-    
+    function closeMobileMenu() {
+      dropdown.classList.remove('is-active');
+      menu.classList.remove('is-active');
+    }
+
+    menuButtons.forEach(function(button) {
+      button.addEventListener('click', function() {
+        closeMobileMenu()
+      });
+    })
     
     $('a').click(function() {
       $('html, body').animate({
           scrollTop: $( $(this).attr('href') ).offset().top - 30
       }, 500);
+      closeMobileMenu();
       return false;
     });
   
@@ -21,14 +31,6 @@ document.onreadystatechange = function () {
       e.stopPropagation();  
       dropdown.classList.toggle('is-active');
       menu.classList.toggle('is-active');
-    });
-
-    menuItem.forEach((item) => {
-      item.addEventListener('click', (e) => {
-        e.stopPropagation();
-        dropdown.classList.remove('is-active');
-        menu.classList.remove('is-active');
-      })
     });
 
     window.onscroll = function () {
